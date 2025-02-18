b3888a0ca13a1fd9d98e378741c722baf854ef07
Maksim Strgogv <alabai.vfrcbv2001@gmail.com>
2025-02-18 15:52:03 +0300

    Исправление

--- homework.py
+++ homework.py
@@ -10,6 +10,7 @@ def test_dark_theme_by_time():
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
     is_dark_theme = current_time.hour >=22 or current_time.hour < 6
+
     assert is_dark_theme is True
 
 
@@ -25,14 +26,12 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = True
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
-    if dark_theme_enabled_by_user:
-        is_dark_theme= dark_theme_enabled_by_user
+    if dark_theme_enabled_by_user is True:
+        is_dark_theme= True
+    elif dark_theme_enabled_by_user is False:
+        is_dark_theme= False
     else:
-        if current_time.hour >=22 or current_time.hour < 6:
-            is_dark_theme= True
-        else:
-            is_dark_theme = False
-
+        is_dark_theme= current_time.hour >=22 or current_time.hour < 6
     assert is_dark_theme is True
 
 
@@ -91,13 +90,16 @@ def print_function_name_and_args(func, *args):
     return f"{func_name} [{args_name}]"
 
 def open_browser(browser_name):
+    print(f"Calling open_browser with browser_name: {browser_name}")
     actual_result = print_function_name_and_args(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 def go_to_companyname_homepage(page_url):
+    print(f"Calling go_to_companyname_homepage with page_url: {page_url}")
     actual_result = print_function_name_and_args(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 def find_registration_button_on_login_page(page_url, button_text):
+    print(f"Calling find_registration_button_on_login_page with page_url: {page_url}, button_text: {button_text}")
     actual_result = print_function_name_and_args(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
